6a35244f1c86e074fe8b9a7176c40e48fcd8ab52
Charissa Johnson <charissa.y.johnson@gmail.com>
2019-09-01 16:43:49 -0500

    add put, patch, and delete

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-routes = (Book) => {
+routes = Book => {
   const bookRouter = express.Router();
   bookRouter
     .route("/books")
@@ -23,14 +23,57 @@ routes = (Book) => {
       });
     });
 
-  bookRouter.route("/books/:bookId").get((req, res) => {
+  // use middleware
+  bookRouter.use("/books/:bookId", (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err) return res.send(err);
-      return res.send(book);
+      // add the book to the request
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
     });
   });
+  bookRouter
+    .route("/books/:bookId")
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.read = req.body.read;
+      req.book.save(err => {
+        if (err) return res.send(err);
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.keys(req.body).map(function(key) {
+        console.log(key) 
+        console.log(req.body[key]);
+        book[key] = req.body[key];
+      });
+      req.book.save(err => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if(err) return res.send(err);
+        return res.sendStatus(204);
+      });
+    })
 
   return bookRouter;
-}
+};
 
 module.exports = routes;
